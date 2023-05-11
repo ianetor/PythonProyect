e0171891074a9e06caab1ad66d3a2127d2841d35
ianetor <88793676+ianetor@users.noreply.github.com>
2023-05-11 09:37:59 -0300

    Update generador_memes.py

--- generador_memes.py
+++ generador_memes.py
@@ -19,8 +19,11 @@ class GeneradorMemes:
             [sg.Image(key="-IMAGE-",size=(300,300))],
         ]       
 
+        relative_path = "botonVolver2.png"
+        boton_volver = os.path.join('./', relative_path)
+        boton_volver= [sg.Button(enable_events=True,image_subsample=(10),button_color='white',border_width=0,image_size=(50,50),image_filename=boton_volver,key='VOLVER')]
 
-        self.layout1=[[sg.Column(self.elegir_imagen,background_color='white'),sg.Column(self.image_viewer_column)]]
+        self.layout1=[[sg.Column(self.elegir_imagen,background_color='white'),sg.Column(self.image_viewer_column),boton_volver]]
         self.window = sg.Window('UNLPImage',self.layout1,size=(800,600),background_color='white')
     def obtener_imagenes(self):
         return [
@@ -29,7 +32,7 @@ class GeneradorMemes:
             if os.path.isfile(os.path.join(self.dir_imagenes, archivo))
                and archivo.lower().endswith((".png", ".gif"))
         ]
-    def abrir_ventana(self):
+    def abrir_ventana(self,perfil_act):
         while True:
             event, values = self.window.read()
             if event == sg.WIN_CLOSED:
@@ -39,13 +42,14 @@ class GeneradorMemes:
             elif event == 'listo':
                 print('hola')
                 selected_files = values['imagenes']
-                print(values['imagenes'])
                 if selected_files:
-                    print(values['imagenes'])
                     filename = os.path.join(self.dir_imagenes, selected_files[0])
-                    print(filename)
                     image= Image.open(filename)
                     image.thumbnail((300,300))
                     image_data = image.tobytes()
                     self.window["-IMAGE-"].update(data=image_data,size=(300,300))
+            elif event == 'VOLVER':
+                self.window.close()
+                menu_ventana = menu_principal_ventana.VentanaMenu(perfil_act)
+                menu_ventana.iniciar_ventana()
         self.window.close()
